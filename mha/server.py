bf951ee2ef9075c1cd9e337dfe51c50df85a032f
Sourcery AI <>
2023-08-26 13:24:44 +0000

    'Refactored by Sourcery'

--- mha/server.py
+++ mha/server.py
@@ -19,7 +19,8 @@ import argparse
 
 app = Flask(__name__)
 reader = geoip2.database.Reader(
-    '%s/data/GeoLite2-Country.mmdb' % app.static_folder)
+    f'{app.static_folder}/data/GeoLite2-Country.mmdb'
+)
 
 
 @app.context_processor
@@ -74,8 +75,7 @@ def dateParser(line):
 
 
 def getHeaderVal(h, data, rex='\s*(.*?)\n\S+:\s'):
-    r = re.findall('%s:%s' % (h, rex), data, re.X | re.DOTALL | re.I)
-    if r:
+    if r := re.findall(f'{h}:{rex}', data, re.X | re.DOTALL | re.I):
         return r[0].strip()
     else:
         return None
@@ -83,44 +83,41 @@ def getHeaderVal(h, data, rex='\s*(.*?)\n\S+:\s'):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        mail_data = request.form['headers'].strip()
-        r = {}
-        n = HeaderParser().parsestr(mail_data)
-        graph = []
-        received = n.get_all('Received')
-        if received:
-            received = [i for i in received if ('from' in i or 'by' in i)]
+    if request.method != 'POST':
+        return render_template('index.html')
+    mail_data = request.form['headers'].strip()
+    r = {}
+    n = HeaderParser().parsestr(mail_data)
+    graph = []
+    received = n.get_all('Received')
+    if received:
+        received = [i for i in received if ('from' in i or 'by' in i)]
+    else:
+        received = re.findall(
+            'Received:\s*(.*?)\n\S+:\s+', mail_data, re.X | re.DOTALL | re.I)
+    c = len(received)
+    for i in range(len(received)):
+        if ';' in received[i]:
+            line = received[i].split(';')
         else:
-            received = re.findall(
-                'Received:\s*(.*?)\n\S+:\s+', mail_data, re.X | re.DOTALL | re.I)
-        c = len(received)
-        for i in range(len(received)):
-            if ';' in received[i]:
-                line = received[i].split(';')
+            line = received[i].split('\r\n')
+        line = list(map(str.strip, line))
+        line = [x.replace('\r\n', ' ') for x in line]
+        try:
+            if ';' in received[i + 1]:
+                next_line = received[i + 1].split(';')
             else:
-                line = received[i].split('\r\n')
-            line = list(map(str.strip, line))
-            line = [x.replace('\r\n', ' ') for x in line]
-            try:
-                if ';' in received[i + 1]:
-                    next_line = received[i + 1].split(';')
-                else:
-                    next_line = received[i + 1].split('\r\n')
-                next_line = list(map(str.strip, next_line))
-                next_line = [x.replace('\r\n', '') for x in next_line]
-            except IndexError:
-                next_line = None
+                next_line = received[i + 1].split('\r\n')
+            next_line = list(map(str.strip, next_line))
+            next_line = [x.replace('\r\n', '') for x in next_line]
+        except IndexError:
+            next_line = None
 
-            org_time = dateParser(line[-1])
-            if not next_line:
-                next_time = org_time
-            else:
-                next_time = dateParser(next_line[-1])
-
-            if line[0].startswith('from'):
-                data = re.findall(
-                    """
+        org_time = dateParser(line[-1])
+        next_time = org_time if not next_line else dateParser(next_line[-1])
+        if line[0].startswith('from'):
+            data = re.findall(
+                """
                     from\s+
                     (.*?)\s+
                     by(.*?)
@@ -130,9 +127,9 @@ def index():
                         (?:\sid\s|$)
                         |\sid\s|$
                     )""", line[0], re.DOTALL | re.X)
-            else:
-                data = re.findall(
-                    """
+        else:
+            data = re.findall(
+                """
                     ()by
                     (.*?)
                     (?:
@@ -142,65 +139,61 @@ def index():
                         |\sid\s
                     )""", line[0], re.DOTALL | re.X)
 
-            delay = (org_time - next_time).seconds
-            if delay < 0:
-                delay = 0
+        delay = (org_time - next_time).seconds
+        delay = max(delay, 0)
+        try:
+            ftime = org_time.utctimetuple()
+            ftime = time.strftime('%m/%d/%Y %I:%M:%S %p', ftime)
+            r[c] = {
+                'Timestmp': org_time,
+                'Time': ftime,
+                'Delay': delay,
+                'Direction': [x.replace('\n', ' ') for x in list(map(str.strip, data[0]))]
+            }
+            c -= 1
+        except IndexError:
+            pass
 
-            try:
-                ftime = org_time.utctimetuple()
-                ftime = time.strftime('%m/%d/%Y %I:%M:%S %p', ftime)
-                r[c] = {
-                    'Timestmp': org_time,
-                    'Time': ftime,
-                    'Delay': delay,
-                    'Direction': [x.replace('\n', ' ') for x in list(map(str.strip, data[0]))]
-                }
-                c -= 1
-            except IndexError:
-                pass
+    for i in list(r.values()):
+        if i['Direction'][0]:
+            graph.append([f"From: {i['Direction'][0]}", i['Delay']])
+        else:
+            graph.append([f"By: {i['Direction'][1]}", i['Delay']])
 
-        for i in list(r.values()):
-            if i['Direction'][0]:
-                graph.append(["From: %s" % i['Direction'][0], i['Delay']])
-            else:
-                graph.append(["By: %s" % i['Direction'][1], i['Delay']])
+    totalDelay = sum(x['Delay'] for x in list(r.values()))
+    fTotalDelay = utility_processor()['duration'](totalDelay)
+    delayed = bool(totalDelay)
 
-        totalDelay = sum([x['Delay'] for x in list(r.values())])
-        fTotalDelay = utility_processor()['duration'](totalDelay)
-        delayed = True if totalDelay else False
+    custom_style = Style(
+        background='transparent',
+        plot_background='transparent',
+        font_family='googlefont:Open Sans',
+        # title_font_size=12,
+    )
+    line_chart = pygal.HorizontalBar(
+        style=custom_style, height=250, legend_at_bottom=True,
+        tooltip_border_radius=10)
+    line_chart.tooltip_fancy_mode = False
+    line_chart.title = f'Total Delay is: {fTotalDelay}'
+    line_chart.x_title = 'Delay in seconds.'
+    for i in graph:
+        line_chart.add(i[0], i[1])
+    chart = line_chart.render(is_unicode=True)
 
-        custom_style = Style(
-            background='transparent',
-            plot_background='transparent',
-            font_family='googlefont:Open Sans',
-            # title_font_size=12,
-        )
-        line_chart = pygal.HorizontalBar(
-            style=custom_style, height=250, legend_at_bottom=True,
-            tooltip_border_radius=10)
-        line_chart.tooltip_fancy_mode = False
-        line_chart.title = 'Total Delay is: %s' % fTotalDelay
-        line_chart.x_title = 'Delay in seconds.'
-        for i in graph:
-            line_chart.add(i[0], i[1])
-        chart = line_chart.render(is_unicode=True)
+    summary = {
+        'From': n.get('From') or getHeaderVal('from', mail_data),
+        'To': n.get('to') or getHeaderVal('to', mail_data),
+        'Cc': n.get('cc') or getHeaderVal('cc', mail_data),
+        'Subject': n.get('Subject') or getHeaderVal('Subject', mail_data),
+        'MessageID': n.get('Message-ID') or getHeaderVal('Message-ID', mail_data),
+        'Date': n.get('Date') or getHeaderVal('Date', mail_data),
+    }
 
-        summary = {
-            'From': n.get('From') or getHeaderVal('from', mail_data),
-            'To': n.get('to') or getHeaderVal('to', mail_data),
-            'Cc': n.get('cc') or getHeaderVal('cc', mail_data),
-            'Subject': n.get('Subject') or getHeaderVal('Subject', mail_data),
-            'MessageID': n.get('Message-ID') or getHeaderVal('Message-ID', mail_data),
-            'Date': n.get('Date') or getHeaderVal('Date', mail_data),
-        }
-
-        security_headers = ['Received-SPF', 'Authentication-Results',
-                            'DKIM-Signature', 'ARC-Authentication-Results']
-        return render_template(
-            'index.html', data=r, delayed=delayed, summary=summary,
-            n=n, chart=chart, security_headers=security_headers)
-    else:
-        return render_template('index.html')
+    security_headers = ['Received-SPF', 'Authentication-Results',
+                        'DKIM-Signature', 'ARC-Authentication-Results']
+    return render_template(
+        'index.html', data=r, delayed=delayed, summary=summary,
+        n=n, chart=chart, security_headers=security_headers)
 
 
 if __name__ == '__main__':
